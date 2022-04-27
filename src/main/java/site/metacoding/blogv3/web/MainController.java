f977ec727e1824d9360071db7fe026d55468b7c8
suhyeon <suhyeon5028@naver.com>
2022-04-27 11:27:22 +0900

    [방문자수 증가 로직 완료]

--- src/main/java/site/metacoding/blogv3/web/MainController.java
+++ src/main/java/site/metacoding/blogv3/web/MainController.java
@@ -7,9 +7,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import site.metacoding.blogv3.domain.post.Post;
 import site.metacoding.blogv3.domain.post.PostRepository;
 
+@Slf4j
 @RequiredArgsConstructor
 @Controller
 public class MainController {
