d3eb98057c7dc8bf553e3dd86dfd3952adddb822
suhyeon <suhyeon5028@naver.com>
2022-05-09 11:14:33 +0900

    [좋아요, 좋아요취소 UI구현 완료1/2]

--- src/main/resources/static/js/post/detail.js
+++ src/main/resources/static/js/post/detail.js
@@ -1,21 +1,45 @@
+function postLikeClick(id) {
+  let isLike = $(`#heart-${id}`).hasClass("my_fake_like");
+  if (isLike) {
+    postUnLike(id);
+  } else {
+    postLike(id);
+  }
+}
+
+function postLike(id) {
+  // fetch();
+  $(`#heart-${id}`).addClass("my_fake_like");
+  $(`#heart-${id}`).removeClass("my_fake_un_like");
+  $(`#heart-${id}`).removeClass("far");
+  $(`#heart-${id}`).addClass("fa-solid");
+}
+
+function postUnLike(id) {
+  // fetch();
+  $(`#heart-${id}`).addClass("my_fake_un_like");
+  $(`#heart-${id}`).removeClass("my_fake_like");
+  $(`#heart-${id}`).removeClass("fa-solid");
+  $(`#heart-${id}`).addClass("far");
+}
+
 // 게시글 삭제, 권한체크 후 삭제 /s/api/post/postId
 $("#btn-delete").click(() => {
-    postDelete();
+  postDelete();
 });
 
 let postDelete = async () => {
+  let postId = $("#postId").val();
+  let pageOwnerId = $("#pageOwnerId").val();
 
-    let postId = $("#postId").val();
-    let pageOwnerId = $("#pageOwnerId").val();
+  let response = await fetch(`/s/api/post/${postId}`, {
+    method: "DELETE",
+  });
 
-    let response = await fetch(`/s/api/post/${postId}`, {
-        method: "DELETE"
-    });
-
-    if (response.status == 200) {
-        alert("삭제성공");
-        location.href = `/user/${pageOwnerId}/post`;
-    } else {
-        alert("삭제실패");
-    }
+  if (response.status == 200) {
+    alert("삭제성공");
+    location.href = `/user/${pageOwnerId}/post`;
+  } else {
+    alert("삭제실패");
+  }
 };
